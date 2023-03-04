bbe04826e14172603c745da3347fe301535e0d28
Anel Button <anelb@tecex.com>
2023-03-04 19:11:23 +0200

    HTML, Update process, Flask process

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, send_from_directory, render_template, Response, url_for, request
 from flask_restful import Resource, Api, reqparse
+from app.updater import UpdateTeams
+import traceback
+import os
+import subprocess
 
 
 app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
@@ -11,8 +15,28 @@ class MSTeamsUpdater(Resource):
         html = render_template('index.html')
         return Response(html, mimetype='text/html')
 
+class UpdateStatus(Resource):
+    def post(self):
+        print(request.form)
+
+        try:
+            updater = UpdateTeams()
+            updater.update_status(status="Busy", message="Testing status update")
+
+            return "Updating Status"
+
+        except Exception as e:
+            return traceback.format_exception(type(e), e, e.__traceback__)
+
 
 api.add_resource(MSTeamsUpdater, '/')
+api.add_resource(UpdateStatus, '/update')
 
 if __name__ == '__main__':
+    
+    subprocess.Popen(["C:\Program Files\Google\Chrome\Application\chrome.exe", '--remote-debugging-port=9222', '--user-data-dir=C:\\selenum\\ChromeProfile'])
+
+    updater = UpdateTeams()
+    updater.local_host()
+    
     app.run(host="localhost", port=8003, debug=True)
